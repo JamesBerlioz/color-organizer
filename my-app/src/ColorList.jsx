e8353adf15f2ea7b74c52a36d2e9cf9cfaa7ffb7
JamesBerlioz <mavlut18@gmail.com>
2023-08-04 22:27:06 +0300

    with the help of hooks it was possible to improve the logic of the application

---
+++ my-app/src/ColorList.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Color from "./Color";
+import { useColors } from "./color-hooks";
+
+export default function ColorList() {
+  const { colors } = useColors();
+  return (
+    <div className="color-list">
+      {colors.map((color) => (
+        <Color key={color.id} {...color} />
+      ))}
+    </div>
+  );
+}
